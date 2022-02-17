3fb53d9ea47a57cfc0f7a77960e5238cdbf3be44
joejiang111 <jyhjoe@gmail.com>
2022-02-17 16:49:59 -0500

    1

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup(){
+    createCanvas(1800, 1000);
+    for(let i = 0; i < 10; i++) {
+
+    }
+
+
+
+
+
+
+
+}
+
+function draw(){
+    background(220);
+
+    
+
+
+    strokeWeight(0);
+    ellipse(500, 450, 80, 80);
+    
+    strokeWeight(40);
+    stroke('blue');
+    ellipse(500, 500, 200, 200);
+
+    
+}
